16b10868344f677e7550628979d7a4787bbd1e91
Jason King <jason.brian.king@gmail.com>
2019-01-19 09:10:22 +0000

    illumos triple

---
+++ src/librustc_target/spec/illumos_base.rs
@@ -0,0 +1,15 @@
+use spec::TargetOptions;
+use std::default::Default;
+
+pub fn opts() -> TargetOptions {
+    TargetOptions {
+        dynamic_linking: true,
+        executables: true,
+        has_rpath: true,
+        target_family: Some("unix".to_string()),
+        is_like_solaris: true,
+	eliminate_frame_pointers: false,
+
+        .. Default::default()
+    }
+}
