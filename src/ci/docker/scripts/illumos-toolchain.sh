58f7d397e99148901865d4bc9cbe63227d2d0a1d
Patrick Mooney <pmooney@pfmooney.com>
2020-03-19 20:53:23 +0000

    XXX fold gcc setup into illumos-toolchain.sh

--- src/ci/docker/scripts/illumos-toolchain.sh
+++ src/ci/docker/scripts/illumos-toolchain.sh
@@ -7,18 +7,23 @@ set -o xtrace
 JOBS="$(getconf _NPROCESSORS_ONLN)"
 
 BINUTILS_VERSION=2.25.1
-GCC_VERSION=4.4.4
-GCC_TARGET=x86_64-sun-solaris2.10
+GCC_VERSION=7.5.0
+BUILD_TARGET=x86_64-sun-solaris2.10
 
 SYSROOT_VER=amd64-20200312-161829
 SYSROOT_URL=https://illumos.org/downloads/sysroot-illumos-$SYSROOT_VER.tar.gz
+SYSROOT_DIR=/ws/sysroot
+
 BINUTILS_BASE=binutils-${BINUTILS_VERSION}
 BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/${BINUTILS_BASE}.tar.bz2
 
+GCC_BASE=gcc-${GCC_VERSION}
+GCC_URL=https://ftp.gnu.org/gnu/gcc/${GCC_BASE}/${GCC_BASE}.tar.xz
+
 case "$1" in
 sysroot)
-	mkdir -p /ws/sysroot
-	cd /ws/sysroot
+	mkdir -p ${SYSROOT_DIR}
+	cd ${SYSROOT_DIR}
 	curl -L "$SYSROOT_URL" | tar xzf -
 	;;
 
@@ -29,12 +34,47 @@ binutils)
 
 	mkdir -p /ws/build/binutils
 	cd /ws/build/binutils
-	/ws/src/binutils/binutils-${BINUTILS_VERSION}/configure \
-	    --target="$GCC_TARGET" \
-	    --with-sysroot="/ws/sysroot"
+	/ws/src/binutils/${BINUTILS_BASE}/configure \
+	    --target="${BUILD_TARGET}" \
+	    --with-sysroot="${SYSROOT_DIR}"
 
 	make -j "$JOBS"
 	make install
+	cd /ws
+	rm -r /ws/src/binutils /ws/build/binutils
+	;;
+
+gcc)
+	mkdir -p /ws/src/gcc
+	cd /ws/src/gcc
+	curl -L "${GCC_URL}" | tar xJf -
+
+	mkdir /ws/build/gcc && \
+	cd /ws/build/gcc && \
+	export CFLAGS="-fPIC"
+	export CXXFLAGS="-fPIC"
+	export CXXFLAGS_FOR_TARGET="-fPIC"
+	export CFLAGS_FOR_TARGET="-fPIC"
+	/ws/src/gcc/${GCC_BASE}/configure \
+	    --target=${BUILD_TARGET} \
+	    --with-sysroot=${SYSROOT_DIR} \
+	    --with-gnu-as \
+	    --with-gnu-ld \
+	    --disable-nls \
+	    --disable-libgomp \
+	    --disable-libquadmath \
+	    --disable-libssp \
+	    --disable-libvtv \
+	    --disable-libcilkrts \
+	    --disable-libada \
+	    --disable-libsanitizer \
+	    --disable-libquadmath-support \
+	    --disable-shared \
+	    --enable-tls
+	make -j "$JOBS"
+	make install
+	cd /ws
+	rm -r /ws/src/gcc /ws/build/gcc
 	;;
 
 *)
